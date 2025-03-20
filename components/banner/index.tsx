2e39bd2be307b7eab26815973a5b51a1cca4f941
Hiago Roedel <hiagojunio4@gmail.com>
2025-03-20 18:03:37 -0300

    first commit react

---
+++ components/banner/index.tsx
@@ -0,0 +1,57 @@
+'use client'
+import { cn } from "@/lib/utils";
+import React from "react";
+import { useEffect, useState } from "react";
+
+export function BannerHome() {
+
+    const text = "Developer Portfolio";
+    const [displayedText, setDisplayedText] = useState("");
+    const [isDeleting, setIsDeleting] = useState(false);
+    const speed = isDeleting ? 50 : 100;
+
+    useEffect(() => {
+        let timeout: NodeJS.Timeout;
+
+        if (!isDeleting && displayedText.length < text.length) {
+            timeout = setTimeout(() => {
+                setDisplayedText(text.substring(0, displayedText.length + 1));
+            }, speed);
+        } else if (isDeleting && displayedText.length > 0) {
+            timeout = setTimeout(() => {
+                setDisplayedText(text.substring(0, displayedText.length - 1));
+            }, speed);
+        } else {
+            timeout = setTimeout(() => {
+                setIsDeleting(!isDeleting);
+            }, 1000);
+        }
+
+        return () => clearTimeout(timeout);
+    }, [displayedText, isDeleting]);
+
+    return (
+        <div className="relative flex h-[100vh] w-full items-center justify-start bg-white dark:bg-black">
+            {/* Grid de fundo */}
+            <div
+                className={cn(
+                    "absolute inset-0",
+                    "[background-size:40px_40px]",
+                    "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+                    "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
+                )}
+            />
+            {/* Gradiente radial para efeito de desfoque nas bordas */}
+            <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+            <div className="container flex-col px-6 md:px-12 flex justify-start items-start">
+                <h1 className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-6xl md:text-8xl lg:text-9xl font-bold text-transparent">
+                    Hiago<br />Roedel
+                </h1>
+                <span className="text-white text-5xl -mt-5 relative z-20">
+                    {displayedText}
+                    <span className="animate-blink">|</span>
+                </span>
+            </div>
+        </div>
+    );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,90 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: [
+      './pages/**/*.{ts,tsx}',
+      './components/**/*.{ts,tsx}',
+      './app/**/*.{ts,tsx}',
+      './src/**/*.{ts,tsx}',
+      './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+      container: {
+        center: true,
+        padding: '2rem',
+        screens: {
+          '2xl': '1400px',
+        },
+      },
+      extend: {
+        colors: {
+          verde: '#59C00B',
+          border: 'hsl(var(--border))',
+          input: 'hsl(var(--input))',
+          ring: 'hsl(var(--ring))',
+          background: 'hsl(var(--background))',
+          foreground: 'hsl(var(--foreground))',
+          primary: {
+            DEFAULT: 'hsl(var(--primary))',
+            foreground: 'hsl(var(--primary-foreground))',
+          },
+          customPurple: '#6424B3',
+          primaryFoore: '#7500FF',
+          secondary: {
+            DEFAULT: 'hsl(var(--secondary))',
+            foreground: 'hsl(var(--secondary-foreground))',
+          },
+          destructive: {
+            DEFAULT: 'hsl(var(--destructive))',
+            foreground: 'hsl(var(--destructive-foreground))',
+          },
+          muted: {
+            DEFAULT: 'hsl(var(--muted))',
+            foreground: 'hsl(var(--muted-foreground))',
+          },
+          accent: {
+            DEFAULT: 'hsl(var(--accent))',
+            foreground: 'hsl(var(--accent-foreground))',
+          },
+          popover: {
+            DEFAULT: 'hsl(var(--popover))',
+            foreground: 'hsl(var(--popover-foreground))',
+          },
+          card: {
+            DEFAULT: 'hsl(var(--card))',
+            foreground: 'hsl(var(--card-foreground))',
+          },
+          unica: {
+            banner: '#7501ff',
+            font: '#ffffff',
+            backModal: 'rgb(69 8 161 / 55%)',
+            sellers: 'rgb(78 3 169)',
+          },
+        },
+        borderRadius: {
+          lg: 'var(--radius)',
+          md: 'calc(var(--radius) - 2px)',
+          sm: 'calc(var(--radius) - 4px)',
+        },
+        keyframes: {
+          'accordion-down': {
+            from: { height: 0 },
+            to: { height: 'var(--radix-accordion-content-height)' },
+          },
+          'accordion-up': {
+            from: { height: 'var(--radix-accordion-content-height)' },
+            to: { height: 0 },
+          },
+        },
+        animation: {
+          'accordion-down': 'accordion-down 0.2s ease-out',
+          'accordion-up': 'accordion-up 0.2s ease-out',
+        },
+        fontFamily: {
+          Krona: ['Krona One', 'sans-serif'],
+        },
+      },
+    },
+    // plugins: [require('tailwindcss-animate')],
+  };
+  
